2fc6e651e00019dd446c667263d8ed27ed6b7983
arneetsinghkalra <a.s.kalra1999@gmail.com>
2021-10-20 19:59:23 -0400

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import Skills from "./Components/Skills/Skills";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 
+import ReactGa from "react-ga";
+import React, { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    ReactGa.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
-      {" "}
       <Navigation />
       <Landing />
       <Education />
